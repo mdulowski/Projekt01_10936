2ac7e020398c07d532ba66771fac061dd0cea029
Marcin Dulowski <marcin.dulowski@microsoft.wsei.edu.pl>
2018-12-15 13:06:21 +0100

    dodanie tabel model i marka

---
+++ Projekt_zaliczeniowy/Projekt_zaliczeniowy/dbo/Tables/marka.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[marka] (
+    [marka_id]    INT           IDENTITY (1, 1) NOT NULL,
+    [marka_nazwa] VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([marka_id] ASC)
+);
+
---
+++ Projekt_zaliczeniowy/Projekt_zaliczeniowy/dbo/Tables/model.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[model] (
+    [model_id]    INT           IDENTITY (1, 1) NOT NULL,
+    [model_nazwa] VARCHAR (100) NULL,
+    PRIMARY KEY CLUSTERED ([model_id] ASC)
+);
+
