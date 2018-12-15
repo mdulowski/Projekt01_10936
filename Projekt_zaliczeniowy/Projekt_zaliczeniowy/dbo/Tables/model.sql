8b167a8fdd40b59e886c5d6142ad480085598c52
Marcin Dulowski <marcin.dulowski@microsoft.wsei.edu.pl>
2018-12-15 13:13:22 +0100

    dodanie kolumny marka_id do tabeli model

--- Projekt_zaliczeniowy/Projekt_zaliczeniowy/dbo/Tables/model.sql
+++ Projekt_zaliczeniowy/Projekt_zaliczeniowy/dbo/Tables/model.sql
@@ -1,6 +1,9 @@
 ﻿CREATE TABLE [dbo].[model] (
     [model_id]    INT           IDENTITY (1, 1) NOT NULL,
     [model_nazwa] VARCHAR (100) NULL,
+    [marka_id]    INT           NULL,
     PRIMARY KEY CLUSTERED ([model_id] ASC)
 );
+
+
 
